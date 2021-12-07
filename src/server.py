f68584cebee8422204f87a6240c420f10582b6bb
rafa0712 <rafamauricio0712@gmail.com>
2021-12-07 16:47:32 -0300

    4 commit

--- src/server.py
+++ src/server.py
@@ -1,25 +1,5 @@
-from pynput.mouse import Listener
-import pyautogui
-print(pyautogui.size())
-def on_move(x, y):
-    print('Pointer moved to {}'.format(
-        (x, y)))
-
-def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Apertou' if pressed else 'Soltou',
-        (x, y)))
-    if not pressed:
-        # Stop listener
-        return False
-
-def on_scroll(x, y, dx, dy):
-    print('Scrolled {0}'.format(
-        (x, y)))
-
-with Listener(
-    on_move=on_move,
-    on_click=on_click,
-    on_scroll=on_scroll
-) as listener:
-    listener.join()
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('', 6081))
+msg = s.recv(2048)
+print(msg)
